ac18ad9b50c96a4237c50b01ed40f93bf08f0b52
Atchuthan Jayakumar <atchuthant.j@gmail.com>
2025-12-22 12:47:33 +0530

    for viewing

---
+++ Arth.java
@@ -0,0 +1,13 @@
+class Arth
+{
+	public static void main(String[] args)
+	{
+		int a=20;
+		int b=6;
+		System.out.println("addittion :"+(a+b));
+		System.out.println("subtraction :"+(a-b));
+		System.out.println("mulitiplication :"+(a*b));
+		System.out.println("division :"+(a/b));
+		System.out.println("modulo :"+(a%b));
+	}
+}
